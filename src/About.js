74b50e2086de685d2418c93a033deaed85ffa823
unknown <76896503@qq.com>
2016-09-18 16:22:35 +0800

    updata

--- src/About.js
+++ src/About.js
@@ -7,21 +7,52 @@ class About extends React.Component {
     super();
     this.state={
       data:{},
-      wait:true
+      wait:true,
+      inputValue:'username'
     }
   }
   componentDidMount(){
-    searchGit()
+    // searchGit()
+    // .then((data)=>{
+    //   console.log(data)
+    //   this.setState({
+    //     data:data.data,
+    //     wait:false
+    //   })
+    //   console.log(this.state.data)
+    // })
+  }
+  handleInput(e){
+    // console.log(e);
+    let value=e.target.value;
+    this.setState({inputValue:value})
+  }
+  handleClick(){
+    let name=this.state.inputValue;
+    console.log(name);
+    searchGit(name)
     .then((data)=>{
       console.log(data)
+      this.setState({
+        data:data.data,
+        wait:false
+      })
+      console.log(this.state.data)
     })
   }
   render () {
+    let gitInfo=(
+      <div>
+        <h3>{this.state.data.login}</h3>
+        <h3>{this.state.data.url}</h3>
+        <img src={this.state.data.avatar_url}/>
+      </div>
+    )
     return(
       <div>
+        <input type='text' value={this.state.inputValue} onChange={this.handleInput.bind(this)}/ ><button onClick={this.handleClick.bind(this)}>搜索</button><br/>
         {
-          this.state.wait ? '正在获取数据' :
-          <img src={this.state.data.avatar_url} />
+          this.state.wait ? '正在获取数据' :gitInfo
         }
       </div>
     )
--- src/Utils/helpers.js
+++ src/Utils/helpers.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 
-function searchGit(){
-  return axios.get('https://api.github.com/users/songyingli')
+function searchGit(add){
+  let address=`https://api.github.com/users/${add}`
+  return axios.get(address)
     .then((res) => ({
       data:res.data
     }))
@@ -11,3 +12,16 @@ function searchGit(){
     });
 }
 export {searchGit}
+
+
+function getJson(){
+  let address=`https://raw.githubusercontent.com/songyingli/demodata/master/card.json?${Math.random()}`
+  return axios.get(address)
+    .then((res) => ({
+      getJson:res.data
+    }))
+    .catch(function (error) {
+      alert(error);
+    });
+}
+export {getJson}
